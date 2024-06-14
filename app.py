b8d0b9074e7a8603149ffbbb40a6d98fd40ad3ba
ykchen <126777396+pesoguin1030@users.noreply.github.com>
2024-06-14 21:55:16 +0800

    fix: response code

--- app.py
+++ app.py
@@ -48,24 +48,6 @@ CORS(app)
 configuration = Configuration(access_token='bLy2uCiWvwIzOxEKvwTwM9e3L+5QkH+1TB2rcESN+fRoflWZqI+pf7jGMks+LO9FcyxQmHrGK01HVUyvlIaGH9pWZ7Tgpauzpda/XfnFpjFJTDmblR+ga4ENSyHRAa1uoevyypVMAgirchnWn6vTLgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('0be1f3b412cb785db1d0af39ee94b81d')
 line_bot_api = LineBotApi('bLy2uCiWvwIzOxEKvwTwM9e3L+5QkH+1TB2rcESN+fRoflWZqI+pf7jGMks+LO9FcyxQmHrGK01HVUyvlIaGH9pWZ7Tgpauzpda/XfnFpjFJTDmblR+ga4ENSyHRAa1uoevyypVMAgirchnWn6vTLgdB04t89/1O/w1cDnyilFU=')
-# 查詢當前點數
-def getCurrentPoint(address, token):
-    url = baseURL + "/carbonExternal/external/getCurrentPoints"
-    data = {
-        "address":address,
-        "token":token
-    }
-    
-    try:
-        response = requests.post(url, json=data)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            print("Error: ", response.status_code)
-            return None
-    except Exception as e:
-        print("Error:", e)
-        return None
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -118,86 +100,41 @@ def verify_token():
 
         # 发送请求到目标API
         response = requests.post(activation_url, json=payload, headers=headers)
+        response = response.json()
 
         # 确认响应
-        if response.status_code == 200:
+        if response["code"] == 200:
+            #return jsonify({"message": "Token verified and linebot activated", "uid": uid}), 200
             return jsonify({"message": "Token verified and linebot activated", "uid": uid}), 200
+        elif response["code"] == 404:
+            return jsonify({"error": "Failed to activate linebot, phone number not found in db"}), response["code"]
         else:
-            return jsonify({"error": "Failed to activate linebot"}), response.status_code
+            return jsonify({"error": "Failed to activate linebot"}), response["code"]
 
 
-        return jsonify({"message": "Token verified", "uid": uid}), 200
+        #return jsonify({"message": "Token verified", "uid": uid}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 400
-
-
-#測試用
-# @app.route('/verify_and_get_phone', methods=['POST'])
-# def verify_token():
-#     # data = request.json
-#     # token = data.get('token')
-#     # #phone_number = data.get('phone_number')
-#     # try:
-#     #     # Check the token's validity using Firebase Admin SDK
-#     #     decoded_token = auth.verify_id_token(token)
-#     #     uid = decoded_token['uid']
-#     #     phone_number = decoded_token['phone_number']
-
-#     #     # Store uid and phone number in the dictionary
-#     #     users_data[uid] = phone_number
-
-#     #     #print(f"UID: {uid}, Phone Number: {phone_number}")
-#     #     # 在这里调用另一个API
-#         activation_url = f"{baseURL}/carbonExternal/external/linebotActivation"
-#     #     phone_number = decoded_token['phone_number']  # 从前面解码的 token 中获取电话号码
-
-#     #     # 檢查是否以'+886'開頭，並替換成'0'
-#     #     if phone_number.startswith('+886'):
-#     #         phone_number = '0' + phone_number[4:]
-
-#     #     payload = {'user_phone': phone_number}  # 准备POST数据
-#     #     headers = {'Content-Type': 'application/json'}
-#         payload = {'user_phone': '0900000000'}  # 准备POST数据
-#         headers = {'Content-Type': 'application/json'}
-#         # 发送请求到目标API
-#         response = requests.post(activation_url, json=payload, headers=headers)
-
-#         # 确认响应
-#         if response.status_code == 200:
-#             #return jsonify({"message": "Token verified and linebot activated", "uid": uid}), 200
-#             return jsonify({"message": "Token verified and linebot activated", "uid": "test"}), 200
-#         else:
-#             return jsonify({"error": "Failed to activate linebot"}), response.status_code
-
-
-#         return jsonify({"message": "Token verified", "uid": uid}), 200
-#     # except Exception as e:
-#     #     return jsonify({"error": str(e)}), 400
-
-# 測試用
-# Additional route to retrieve stored data (for example purposes)
-# @app.route('/get_user_data/<uid>', methods=['GET'])
-# def get_user_data(uid):
-#     phone_number = users_data.get(uid, None)
-#     if phone_number:
-#         return jsonify({"uid": uid, "phone_number": phone_number}), 200
-#     else:
-#         return jsonify({"error": "User not found"}), 404
 
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
 
     # 准备要发送给 Node.js API 的数据
     if event.message.text == "點數查詢":
-        userAddress = "0x921db87f8e0a889e8f1c245ebae96b179be12605"
-        userToken = "U2FsdGVkX1/gbFPx27PaO3n8LhUAaVV/G0uN+ujhiqQ="
+        # userAddress = "0x921db87f8e0a889e8f1c245ebae96b179be12605"
+        # userToken = "U2FsdGVkX1/gbFPx27PaO3n8LhUAaVV/G0uN+ujhiqQ="
 
         # 调用 Node.js API
-        response = getCurrentPoint(userAddress, userToken)
+        #response = getCurrentPoint(userAddress, userToken)
+        response = requests.post(baseURL + "/carbonExternal/external/getPointByLineID", json={"line_user_id": event.source.user_id}).json()
         
         if response:
             # 处理 Node.js API 的响应
-            replyContent = "在錢包地址: \n" + userAddress + "\n中存有: " + response["message"] + " 點碳權點數。"
+            #replyContent = "在錢包地址: \n" + userAddress + "\n中存有: " + response["message"] + " 點碳權點數。"
+            if response["type"] == "balance":
+                replyContent = "在錢包地址: \n" + response["address"] + "\n中存有: " + response["message"] + " 點碳權點數。"
+            elif response["type"] == "temp_points":
+                replyContent = "您目前尚未綁定錢包, 於手機號碼 " + str(response["phone"])+" 中存有 " + str(response["message"]) + " 點暫存點數"
             with ApiClient(configuration) as api_client:
                 line_bot_api = MessagingApi(api_client)
                 line_bot_api.reply_message_with_http_info(
@@ -208,15 +145,16 @@ def handle_message(event):
                 )
         else:
             print("Failed to call Node.js API")
-    elif event.message.text == "請點擊以下連結驗證手機號碼":
+    elif event.message.text == "手機綁定":
         user_id = event.source.user_id
+        replyContent = "請點擊以下連結進行簡訊驗證\n"
         verification_url = rf"{sms_react_url}/{user_id}"
         with ApiClient(configuration) as api_client:
             line_bot_api = MessagingApi(api_client)
             line_bot_api.reply_message_with_http_info(
                 ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=verification_url)]
+                messages=[TextMessage(text=replyContent+verification_url)]
                 )
             )
 
@@ -233,3 +171,4 @@ if __name__ == "__main__":
 #
 # 可使用ngrok
 # 測試時手機有連到電腦的wifi，但可能不用
+# 需要./ngrok http 192.168.0.105:5000  並且把 192.168.0.105:5000/callback 丟到LineBot後台 webhook url
